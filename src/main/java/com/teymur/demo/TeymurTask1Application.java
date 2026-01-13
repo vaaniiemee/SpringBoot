a13abee7b9ecdf9dda34405162121a422aab2054
vaaniiemee <tyahyayev0@gmail.com>
2026-01-13 15:00:18 +0100

    Initial commit

---
+++ src/main/java/com/teymur/demo/TeymurTask1Application.java
@@ -0,0 +1,13 @@
+package com.teymur.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeymurTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeymurTask1Application.class, args);
+	}
+
+}
